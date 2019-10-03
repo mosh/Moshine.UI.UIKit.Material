cec0a03fbed914b51db4de68d4b2696ce801e4e8
John Moshakis <john.moshakis@firmex.com>
2019-10-03 00:31:08 -0400

    floating button method

---
+++ MaterialExtensions.pas
@@ -0,0 +1,34 @@
+﻿namespace Moshine.UI.UIKit.Material;
+
+uses
+  MaterialComponents,
+  PureLayout,
+  UIKit;
+
+type
+  MaterialExtensions = public extension class(UIViewController)
+  public
+
+    method addFloatingButton(imageNamed:String):MDCFloatingButton;
+    begin
+      var addImage := UIImage.imageNamed(imageNamed).ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);;
+
+      var someButton := new MDCFloatingButton;
+
+      someButton.setImage(addImage) forState(UIControlState.Normal);
+
+      someButton.frame := CGRectMake(0, 0, 48, 48);
+
+      self.view.addSubview(someButton);
+
+      //method autoPinEdgeToSuperviewEdge(edge: PureLayout.ALEdge) withInset(inset: CGFloat): not nullable NSLayoutConstraint; public;
+
+
+      someButton.autoPinEdgeToSuperviewEdge(ALEdge.Bottom) withInset(100);
+      someButton.autoPinEdgeToSuperviewEdge(ALEdge.Right) withInset(100);
+
+      exit someButton;
+
+    end;
+  end;
+end.
