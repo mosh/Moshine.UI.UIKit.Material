9caa6688e4b8ec1b95e7508300d8688edaf3e6ee
John Moshakis <john.moshakis@firmex.com>
2019-10-03 22:00:58 -0400

    Update MaterialExtensions.pas

--- MaterialExtensions.pas
+++ MaterialExtensions.pas
@@ -9,19 +9,17 @@ type
   MaterialExtensions = public extension class(UIViewController)
   public
 
-    method addFloatingButton(imageNamed:String):MDCFloatingButton;
+    method addFloatingButton(imageNamed:String; bottomOffset:Integer := 100; rightOffset:Integer := 50):MDCFloatingButton;
     begin
-      var addImage := UIImage.imageNamed(imageNamed).ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);;
+      var addImage := UIImage.imageNamed(imageNamed).ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
 
       var someButton := new MDCFloatingButton;
 
       someButton.setImage(addImage) forState(UIControlState.Normal);
       someButton.frame := CGRectMake(0, 0, 48, 48);
       self.view.addSubview(someButton);
-      someButton.autoPinEdgeToSuperviewEdge(ALEdge.Bottom) withInset(100);
-      someButton.autoPinEdgeToSuperviewEdge(ALEdge.Right) withInset(50);
-      //someButton.autoAlignAxisToSuperviewAxis(ALAxis.horizontal);
-
+      someButton.autoPinEdgeToSuperviewEdge(ALEdge.Bottom) withInset(bottomOffset);
+      someButton.autoPinEdgeToSuperviewEdge(ALEdge.Right) withInset(rightOffset);
       exit someButton;
 
     end;
