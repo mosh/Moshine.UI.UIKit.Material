4872b6d0c10b0ca43892918f6dce8fae78218b6a
John Moshakis <john.moshakis@firmex.com>
2021-03-17 00:37:57 -0400

    left and right offset

--- MaterialExtensions.pas
+++ MaterialExtensions.pas
@@ -9,7 +9,7 @@ type
   MaterialExtensions = public extension class(UIViewController)
   public
 
-    method addFloatingButton(imageNamed:String; bottomOffset:Integer := 100; rightOffset:Integer := 50):MDCFloatingButton;
+    method addFloatingButton(imageNamed:String) withOffsetRight(bottomOffset:Integer := 100; rightOffset:Integer := 50):MDCFloatingButton;
     begin
       var addImage := UIImage.imageNamed(imageNamed).imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
 
@@ -23,5 +23,23 @@ type
       exit someButton;
 
     end;
+
+    method addFloatingButton(imageNamed:String) withOffsetLeft(bottomOffset:Integer := 100; leftOffset:Integer := 50):MDCFloatingButton;
+    begin
+      var addImage := UIImage.imageNamed(imageNamed).imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
+
+      var someButton := new MDCFloatingButton;
+
+      someButton.setImage(addImage) forState(UIControlState.Normal);
+      someButton.frame := CGRectMake(0, 0, 48, 48);
+      self.view.addSubview(someButton);
+      someButton.autoPinEdgeToSuperviewEdge(ALEdge.Bottom) withInset(bottomOffset);
+      someButton.autoPinEdgeToSuperviewEdge(ALEdge.Left) withInset(leftOffset);
+      exit someButton;
+
+    end;
+
+
+
   end;
 end.
